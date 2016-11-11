57b754313c2934d9eaa22126d626ed800969d6f6
giseli16 <astrid_rguez21@outlook.com>
2016-11-11 04:26:59 -0600

    Se agregó la interfaz y la superclase principal

---
+++ src/AritmeticaYExponentes/Operaciones.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package AritmeticaYExponentes;
+
+/**
+ *
+ * @author giselle
+ */
+public interface Operaciones {
+    
+    /*Los parámetros a recibir se definen como Object ya que se sumarán polinomios, complejos entre otros, es decir, no será de un sólo tipo.*/
+    Object suma(Object a, Object b);
+    Object multiplicacion(Object a, Object b);
+    Object division(Object a, Object b); 
+    Object exponente(Object a, double b);/*En el caso del exponente, siempre será un entero o una fracción, por lo que se define como double*/
+    Object raiz(Object a, Object b);
+}
--- src/AritmeticaYExponentes/OperacionesSimples.java
+++ src/AritmeticaYExponentes/OperacionesSimples.java
@@ -3,29 +3,87 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Teclado;
+package AritmeticaYExponentes;
+
+import static java.lang.Math.pow;
 
 /**
  *
  * @author ricardo
  */
-public class Operaciones {
-    // aqui se van a poner las operaciones suma resta y multiplicacion
+public class OperacionesSimples implements Operaciones {
+    //Aquí se van a poner las operaciones básicas para los números reales
     
-    public void suma(){
+    private double num1;
+    private double num2;
+
+    public OperacionesSimples(Object num1, Object num2) {
+        this.num1 = (double)num1;
+        this.num2 = (double)num2;
+    }
+    
+    
+    
+
+    @Override
+    public Object suma(Object a, Object b) {
+         this.num1= (double)a;
+         this.num2= (double)b;
+         
+         return num1+num2;
+    }
+
+    @Override
+    public Object multiplicacion(Object a, Object b) {
+         this.num1= (double)a;
+         this.num2= (double)b;
+         
+         return num1*num2;
+       
+    }
+
+    @Override
+    public Object division(Object a, Object b) {
+        this.num1= (double)a;
+        this.num2= (double)b;
+         
+        return num1/num2;
+        //Falta poner un try catch
+    }
+
+    @Override
+    public Object exponente(Object a, double b) {
+        this.num1= (double)a;
+        this.num2= (double)b;
+         
+        return pow(num1, num2);
+    }
+
+    @Override
+    public Object raiz(Object a, Object b) {
+        this.num1= (double)a;
+        this.num2= (double)(1/(double)b);
+         
+        return pow(num1, num2);
         
     }
     
-    public void resta(){
+    public Object alCuadrado(Object a){
+        this.num1= (double)a;
+        
+        return pow(num1,2);
+        
         
     }
     
-    public void multiplicacion(){
+    public Object raizCuadrada(Object a){
+        this.num1= (double)a;
+        
+        return pow(num1,1/2);
         
     }
+   
     
-    public void division(){
-        
-    }
+    
     
 }
