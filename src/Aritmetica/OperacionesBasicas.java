6a691f945dd23538cc91ad4cce531636ca511cc6
Ricardo Canul <ricardocanul7@gmail.com>
2016-11-12 17:39:45 -0600

    se ordenaron los tipos de numeros en un paquete

---
+++ src/Aritmetica/OperacionesBasicas.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Aritmetica;
+
+import Aritmetica.TipoNumero.Real;
+
+/**
+ *
+ * @author ricardo
+ */
+public class OperacionesBasicas implements Operaciones
+{
+
+    @Override
+    public Object suma(Object a, Object b) {
+        return ((Real)a).getNumero() + ((Real)b).getNumero();
+    }
+
+    @Override
+    public Object multiplicacion(Object a, Object b) {
+        return ((Real)a).getNumero() * ((Real)b).getNumero();
+    }
+
+    @Override
+    public Object division(Object a, Object b) {
+        return ((Real)a).getNumero() / ((Real)b).getNumero();
+    }
+
+    @Override
+    public Object exponente(Object a, double b) {
+        return Math.pow(((Real)a).getNumero(), b);
+    }
+
+    @Override
+    public Object raiz(Object a, int b) {
+        return Math.pow(((Real)a).getNumero(), (1 / b));
+    }
+    
+    
+}
