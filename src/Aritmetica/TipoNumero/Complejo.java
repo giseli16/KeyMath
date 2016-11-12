01af0e3094743bbbaff8a79066e273161299f93a
Ricardo Canul <ricardocanul7@gmail.com>
2016-11-12 17:42:32 -0600

    se ordenaron los tipos de numeros en un paquete

---
+++ src/Aritmetica/TipoNumero/Complejo.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Aritmetica.TipoNumero;
+
+/**
+ *
+ * @author ricardo
+ */
+public class Complejo extends Real
+{
+    private double imaginario;
+
+    public Complejo(double numero, double imaginario) {
+        super(numero);
+        this.imaginario = imaginario;
+    }
+    
+    
+
+    public double getImaginario() {
+        return imaginario;
+    }
+
+    public void setImaginario(double imaginario) {
+        this.imaginario = imaginario;
+    }
+    
+    
+}
---
+++ src/Aritmetica/TipoNumero/Real.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Aritmetica.TipoNumero;
+
+/**
+ *
+ * @author ricardo
+ */
+public class Real{
+    //Aquí se van a poner las operaciones básicas para los números reales
+    
+    private double numero;
+
+    // Constructor
+    public Real(double numero) {
+        this.numero = numero;
+    }
+    
+    
+    // Getters and Setters
+    public double getNumero() {
+        return numero;
+    }
+    
+    public void setNumero(double numero) {
+        this.numero = numero;
+    }
+    
+}
